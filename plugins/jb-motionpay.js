5a09f423db94be124279f3d8d776e3846a9fd1c9
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-25 00:32:27 +0700

    v5

--- plugins/jb-motionpay.js
+++ plugins/jb-motionpay.js
@@ -8,6 +8,16 @@ let text = `${htki} MOTION PAY ${htka}
 📞 Nomor: 088215689772
 👤 A/n: Fangz
 📦 Mitra: Fangz BOT
+💰 Metode pembayaran: Online ( ~Cod~ )let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `${htki} MOTION PAY ${htka}
+
+👋‘‹, ingin melanjutkan pembayaran?
+
+💌 *Pembayaran*
+💳 Via: Motion Pay
+📞 Nomor: ---
+👤 A/n: Vinz
+📦 Mitra: xʜɪᴍᴍᴇʀ ʙᴏᴛ࿐
 💰 Metode pembayaran: Online ( ~Cod~ )
 
 📣 *Beli*
@@ -28,6 +38,39 @@ Pastikan saldo kamu mencukupi untuk bertransaksi!
 `
 const templateButtons = [
     {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.command = /^motionpay$/i
+handler.private = true
+
+export default handler
+
+📣 *Beli*
+
+Apk TokoBot: https://app.bukaolshop.com/toko/tokobot
+
+Apk FangzApp:https://web.jagel.id/store/fangzapp
+
+Toko Website:https://toko.ly/TokoBot   (Non apk)
+
+✏️ _Informasi Pembayaran_
+
+Pembayaran Sewa hanya dapat menggunakan saldo.
+Pastikan saldo kamu mencukupi untuk bertransaksi!
+
+
+❗KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: 'QRIS', url: 'Error'}},
     {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
 ]
 let tm = {
