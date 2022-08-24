87cf654261184f1659dc3ab104b6057f3a79f417
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-25 00:07:34 +0700

    v5

--- plugins/jb-dana.js
+++ plugins/jb-dana.js
@@ -27,7 +27,7 @@ Pastikan saldo kamu mencukupi untuk bertransaksi!
 ❗KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 1, urlButton: {displayText: 'QRIS', url: 'Error'}},
     {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
 ]
 let tm = {
