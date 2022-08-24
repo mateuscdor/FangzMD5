b2f58d064b29196981524fa396c0b69912c0ba85
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-24 23:46:31 +0700

    v5

---
+++ plugins/_autosetbiobot.js
@@ -0,0 +1,25 @@
+let handler = m => m
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
+	let _muptime
+if (new Date() * 1 - setting.status > 1000) {
+        let _uptime = process.uptime() * 1000
+        let uptime = clockString(_uptime)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | ${xʜɪᴍᴍᴇʀ} by ${VinzCyx}`).catch(_ => _)
+        setting.status = new Date() * 1
+    }
+
+}
+
+export default handler
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
