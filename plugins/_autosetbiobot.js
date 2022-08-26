0cec4a52872bd65cd8b08d989a7efb9c0f9087d6
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-26 18:50:20 +0700

    v5

--- plugins/_autosetbiobot.js
+++ plugins/_autosetbiobot.js
@@ -1,27 +1,23 @@
-import fetch from 'node-fetch'
-    let handler = async 
+export async function all(m) {
+	let setting = db.data.settings[this.user.jid]
 
-// update status
-    if (set.autoupdatestatus) {
-        if (new Date() * 1 - set.status > 1000) {
-            let _uptime = process.uptime() * 1000
-            let uptime = conn.clockString(_uptime)
-            await this.setStatus(`Aktif selama ${uptime} | Mode: ${set.self ? 'Private' : set.group ? 'Hanya Grup' : 'Publik'} | stikerinbot oleh ariffb`).catch(_ => _)
-            set.status = new Date() * 1
-        }
-    }
+	if (new Date() * 1 - setting.status > 1000) {
+		let _uptime = process.uptime() * 1000
+		let uptime = clockString(_uptime);
+		let wm = 'xʜɪᴍᴍᴇʀ ʙᴏᴛ࿐'
+		let bio = `❗Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} | 🔥 Sc By ${wm}`
 
+		await this.setBio(bio).catch(_ => _)
+		setting.status = new Date() * 1
+	}
 }
 
-export default handler
-    
 function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+	let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+	let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+	let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+	return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
-}
+
+// buatan FokusDotId (Fokus ID)
